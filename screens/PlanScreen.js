533ee68cf98abd0e36f48efbbe8c2c8a3dc6a7e4
EvgDeGa <dorokhovjug@gmail.com>
2020-04-16 20:38:08 +0700

    Add

---
+++ screens/PlanScreen.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Button,
+  TextInput,
+} from "react-native";
+import { Navbar } from "../components/Navbar";
+
+export default PlanScreen = (props) => {
+  return (
+    <View>
+      <Navbar title="Plan" />
+      <View></View>
+    </View>
+  );
+};
